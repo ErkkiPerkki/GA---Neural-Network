f2eef91f36d1b0891180529aa2f5d87974c9e358
ErkkiPerkki <erkkiperkkibtw@gmail.com>
2024-10-01 12:31:54 +0200

    nämen där ser man en neuron
    jävla skit program FUCK c# 7.0

---
+++ Classes/Neuron.cs
@@ -0,0 +1,17 @@
+﻿namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        private float _Value;
+
+        public float Value
+        {
+            get { return _Value; }
+        }
+
+        public Neuron()
+        {
+            _Value = (float)Program.random.NextDouble();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace NeuralNetwork
+﻿using System;
+
+namespace NeuralNetwork
 {
     public class Program
     {
+        public static Random random = new Random();
+
         static void Main()
         {
+            Neuron tomte = new Neuron();
+            tomte.Value
         }
     }
 }
